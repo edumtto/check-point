70963e6c5381d3ffc34f7651ffdd6cd6a3679fd0
Eduardo Oliveira <edumottaoliveira@gmail.com>
2024-02-27 20:48:53 -0800

    Replace main Side Menu with a Tab Menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,71 +1,41 @@
 'use client'
-import React, { useState } from 'react'
+import React from 'react'
 import styles from './page.module.css'
 import { ActivitiesScene } from './scenes/activities/activities'
 import { MembersScene } from './scenes/members/members'
-import { MainContainer, SceneHeader } from './globals/components/global-components'
-import { Menu, Layout } from 'antd'
+import { MainContainer } from './globals/components/global-components'
+import { Tabs } from 'antd'
 import { database } from './globals/database'
-const { Sider, Content } = Layout
 
 export default function Home (): JSX.Element {
-  const [selectedMenuItem, setSelectedMenuItem] = useState('0')
-  let mainTitle = 'Activities'
-
-  const menuItems: any[] = [
+  const items = [
     {
-      key: 'g0',
-      label: 'Check In',
-      type: 'group',
-      children: [
-        { key: '0', label: 'Activities' },
-        { key: '1', label: 'Members' }
-      ]
+      key: '0',
+      label: 'Activities',
+      children: <ActivitiesScene activities={database.activities} />
     },
     {
-      key: 'g1',
-      label: 'Manage',
-      type: 'group',
-      children: [
-        { key: '2', label: 'Activities' },
-        { key: '3', label: 'Members' }
-      ]
+      key: '1',
+      label: 'Members',
+      children: <MembersScene members={database.members} />
     },
     {
-      key: 'g2',
+      key: '2',
+      label: 'Analytics',
+      children: 'Analytics data'
+    },
+    {
+      key: '3',
       label: 'About',
-      type: 'group',
-      children: [
-        { key: '4', label: 'FAQ' },
-        { key: '5', label: 'Support' }
-      ]
+      children: 'Version 0.10'
     }
   ]
 
-  let mainScene: React.JSX.Element
-  if (selectedMenuItem === '0') {
-    mainTitle = 'Activities'
-    mainScene = <ActivitiesScene activities={database.activities} />
-  } else {
-    mainTitle = 'Members'
-    mainScene = <MembersScene members={database.members} />
-  }
-
   return (
     <MainContainer>
-      <Sider className={styles['side-menu']}>
-        <Menu className={styles['side-menu-content']} onClick={onMenuItemClick} defaultSelectedKeys={['0']} mode="inline" items={menuItems} />
-      </Sider>
-      <Content>
-        <SceneHeader title={mainTitle} showBackButton={false} handleBackButtonClick={() => undefined} />
         <div className={styles['selected-scene']}>
-          {mainScene}
+          <Tabs defaultActiveKey="0" items={items}/>
         </div>
-      </Content>
     </MainContainer>
   )
-
-  function onMenuItemClick (item: any): void {
-    setSelectedMenuItem(item.key)
-  }
 }
